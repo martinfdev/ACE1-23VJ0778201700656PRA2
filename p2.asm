b1258aa894db3e4a862cca33359a4445ac9f62d2
pedro <pedromartinf07@gmail.com>
2023-06-24 19:41:11 -0600

    get code prod, validate length and characters

--- p2.asm
+++ p2.asm
@@ -14,8 +14,8 @@
     opciones_m                  DB "=======OPCIONES MENU=======", 0A, "$"
     opciones_producto           DB "====+OPCIONES PRODUCTO+====", 0A, "$"
     productos_m                 DB "(p) Productos", 0A, "$"
-    ingreso_prod_m              DB "(i) Ingresar Productos", 0A, "$"
-    eliminar_prod_m             DB "(e) Eliminar Productos", 0A, "$"
+    ingreso_prod_m              DB "(c) Crear Producto", 0A, "$"
+    eliminar_prod_m             DB "(e) Eliminar Producto", 0A, "$"
     ver_prod_m                  DB "(b) Ver Productos", 0A, "$"
     ventas_m                    DB "(v) Ventas", 0A, "$"
     herramientas_m              DB "(h) Herramientas", 0A, "$"
@@ -24,18 +24,27 @@
     msg2                        DB "Opcion incorrecto, intenta de nuevo!", 0A, "$"
     file_acces                  DB "PRA2.CNF",00
     msg_error_open_file_access  DB "Error al abrir el archivo de configuracion", 0A, "Programa terminado!", 0A, "$"
-    msg_error_create_file_base    DB 0A,0A, "Error al crear el archivo de productos!", 0A, "$"
+    msg_error_create_file_base  DB 0A,0A, "Error al crear el archivo de productos!", 0A, "$"
     creden_usuario_clave        DB "[credenciales]", "usuario=", 22, "pfrancisco", 22, "clave=", 22, "201700656", 22, 00
     msg_error_creden            DB "Error de autenticacion", 0A, "Programa terminado!", 0A, "$"
     msg_correcto_cred           DB "Credenciales correctas!", 0A, "Presione Enter para continuar: $"
     msg_escape_menu_principal   DB "(Esc) Volver a Menu Principal", 0A, "$"
+    msg_codigo_invalido         DB 0A, "Codigo invalido! $"
     file_product                DB "PROD.BIN", 00
-    temp_buffer                 DB 040 DUP(0), "$"
+    temp_buffer                 DB 040 DUP(0)
     handle_file                 DW ?
     handle_file_prod            DW ?
-    buffer_file                 DB 040 DUP(0), "$"
+    buffer_file                 DB 060 DUP(0)
     buffer_teclado              DB 20, 00
-                                DB 100h DUP(?)
+                                DB 21 DUP(0)
+    prod_codigo_name            DB 0A, "Codigo: $"
+    prod_descrip_name           DB 0A, "Descripcion: $"
+    prod_precio_name            DB 0A, "Precio: $"
+    prod_unidades_name         	DB 0A, "Unidades: $"
+    buff_prod_codigo            DB 04 (0)
+    buff_prod_descrip           DB 40 (0)
+    buff_prod_precio            DB 01(0)
+    buff_prod_unidades         	DB 01(0)
 
 ;segmento de codigo
 .code
@@ -250,17 +259,16 @@ productos:
     mov DX, offset file_product
     mov AH, 3D
     int 21
-    int 03
     ;mensaje de error del archivo
     jc crear_archivo ;  se crea el archivo si no existe
-    mov [handle_file_prod], AX ;abdle no cambia
+    mov [handle_file_prod], AX ;handle cuando se crea o se habre el archivo 
 
     ;leer opcion de usuario
     mov AH, 07
     int 21
 
-    cmp AL, 069 ;i
-    ;je teclado_correcto
+    cmp AL, 063 ;c
+    je crear_producto
     cmp AL, 065 ;e
     ;je teclado_correcto
     cmp AL, 076 ;v
@@ -281,8 +289,111 @@ error_crear_archivo:
     mov DX, offset msg_error_create_file_base
     mov AH, 09
     int 21
-    
     jmp menu_loop
+
+crear_producto:
+    mov DX, offset salto
+    mov AH, 09
+    int 21
+pedir_codigo:    
+    mov DX, offset prod_codigo_name
+    mov AH,09
+    int 21
+    ;entrada en teclado!
+    mov DX, offset buffer_teclado
+    mov AH, 0A
+    int 21
+    ;comprobar la cantidad de caracteres >= 4
+    mov DI, offset buffer_teclado
+    inc DI
+    mov AL, [DI]
+    cmp AL, 00 ;buffer es igual cero volver a pedir
+    je pedir_codigo
+    cmp AL, 05
+    jb validar_caracteres_codigo ;condigo aceptado
+    jmp pedir_codigo
+
+validar_caracteres_codigo:
+    mov SI, offset buffer_teclado
+    inc SI
+    mov CH, 00
+    mov CL, [SI] ;copiar cantidad de cadenas en el buffer teclado
+    inc SI ;incrementar SI para poscionarse a la primera cadena a validar
+
+;validar los caracteres [A-Z0-9]
+validar_cadena_codigo:
+    mov AL, [SI]
+    cmp AL, 41 ; 'A'
+    jb validar_numeros_cod ;caracteres menores a A para buscar numeros
+    cmp AL, 5A ; 'Z'
+    ja print_caracter_invalido_codigo ;saltar msg caracter invalido mayor que Z
+    jmp siguiente_caracter_codigo
+
+validar_numeros_cod:
+    cmp AL, 30 ;'0'
+    jb  print_caracter_invalido_codigo
+    cmp AL, 3A ;':'
+    je  print_caracter_invalido_codigo
+    cmp AL, 3B ;';'
+    je  print_caracter_invalido_codigo
+    cmp AL, 3C ;'<'
+    je  print_caracter_invalido_codigo
+    cmp AL, 3D ;'='
+    je  print_caracter_invalido_codigo
+    cmp AL, 3E ;'>'
+    je  print_caracter_invalido_codigo
+    cmp AL, 3F ;'?'
+    je  print_caracter_invalido_codigo
+    cmp AL, 40 ;'@'
+    je  print_caracter_invalido_codigo
+    cmp AL, 41 ;'A'
+    ja validar_cadena_codigo
+    jmp siguiente_caracter_codigo
+
+siguiente_caracter_codigo:
+    inc SI
+    loop validar_cadena_codigo
+    jmp copiar_a_buf_codigo
+
+print_caracter_invalido_codigo:
+    mov DX, offset msg_codigo_invalido
+    mov AH, 09
+    int 21
+    jmp pedir_codigo
+
+copiar_a_buf_codigo:
+    mov DI, offset buff_prod_codigo
+    mov SI, offset buffer_teclado
+    inc SI
+    mov CH, 00
+    mov CL, [SI]
+    inc SI
+    mov AH, 00    
+ciclo_copiar_codigo:
+    mov AL, [SI]
+    mov [DI], AL
+    inc SI
+    loop ciclo_copiar_codigo
+
+
+
+
+
+
+    ; mov DX, offset prod_descrip_name
+    ; mov AH,09
+    ; int 21    
+    ; mov DX, offset prod_precio_name
+    ; mov AH,09
+    ; int 21
+    ; mov DX, offset prod_unidades_name
+    ; mov AH,09
+    ; int 21    
+    jmp productos
+; pedir_codigo:
+;     mov 
+    
+
 
 fin:
 .EXIT
